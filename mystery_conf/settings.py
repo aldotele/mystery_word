79266480a46384772b7260da8842f370ab5ae3a3
aldotele <aldo.telese@hotmail.it>
2021-06-03 16:31:49 +0200

    added heroku deploy boilerplate

--- mystery_conf/settings.py
+++ mystery_conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path  # added
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -131,3 +132,5 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
