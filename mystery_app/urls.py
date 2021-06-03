dc4f7dd394af74d2fb2755eb2df8d2ad4bc76d01
aldotele <aldo.telese@hotmail.it>
2021-06-03 11:28:26 +0200

    general improvement: refactoring, instruction section, reusing context variables

--- mystery_app/urls.py
+++ mystery_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from mystery_app.views import index, show_five_words, end_game
+from mystery_app.views import index, play_round, end_game, index_instructions
 
 urlpatterns = [
     path('', index, name='index'),
-    path('play', show_five_words, name='play'),
+    path('instructions', index_instructions, name='index_instructions'),
+    path('play', play_round, name='play'),
     #path('play', GuessCreateView, name='play'),
     #path('end/<int:pk>', end_game, name='end'),
     path('end', end_game, name='end')
