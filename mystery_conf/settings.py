aecfc0b9a66b0c202b867d12a5a5f733c33206af
aldotele <aldo.telese@hotmail.it>
2021-06-04 10:55:59 +0200

    added staticfiles to dirs

--- mystery_conf/settings.py
+++ mystery_conf/settings.py
@@ -127,9 +127,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # added
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
