cf6f4129244d4f17e0e6a13a4ac7eeaed006aeaf
aldotele <aldo.telese@hotmail.it>
2021-10-10 16:12:27 +0200

    slash appended, debug False

--- mystery_app/urls.py
+++ mystery_app/urls.py
@@ -3,9 +3,9 @@ from mystery_app.views import index, play_round, end_game, index_instructions
 
 urlpatterns = [
     path('', index, name='index'),
-    path('instructions', index_instructions, name='index_instructions'),
-    path('play', play_round, name='play'),
+    path('instructions/', index_instructions, name='index_instructions'),
+    path('play/', play_round, name='play'),
     #path('play', GuessCreateView, name='play'),
     #path('end/<int:pk>', end_game, name='end'),
-    path('end', end_game, name='end')
+    path('end/', end_game, name='end')
 ]
