0eb0d3f261796a1069f82428658d78481ece5297
aldotele <aldo.telese@hotmail.it>
2021-10-10 16:08:04 +0200

    debug False

--- mystery_conf/settings.py
+++ mystery_conf/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['*']
 
